4674d1b0c8eb36c0e62e8ce5ef9a68ef26c3fb35
AlexSSY <adolf5341@gmail.com>
2025-12-27 22:43:14 +0200

    add auth interfaces

--- app/features/auth/interfaces.py
+++ app/features/auth/interfaces.py
@@ -1,4 +1,5 @@
 from typing import Protocol, runtime_checkable
+from ..users.models import User
 
 
 @runtime_checkable
@@ -6,3 +7,15 @@ class Authenticable(Protocol):
     def login(self) -> str: ...
     def password_hash(self) -> str: ...
     def is_active(self) -> bool: ...
+
+
+@runtime_checkable
+class AuthBackend(Protocol):
+    async def authenticate(self, request) -> User | None:
+        raise NotImplementedError
+
+
+@runtime_checkable
+class UserProvider(Protocol):
+    async def get_by_id(self, user_id: int) -> Authenticable | None: ...
+    async def get_by_login(self, login: str) -> Authenticable | None: ...
---
+++ app/features/users/models.py
@@ -0,0 +1,6 @@
+from app.core.database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+    
